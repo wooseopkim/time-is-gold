ec7957947e0f35aa44d24915ee03615a318952e4
Wooseop Kim <wooseop.kim.dev@gmail.com>
2022-01-09 19:13:26 +0900

    Add OthersPage test

---
+++ src/app/pages/OthersPage.test.tsx
@@ -0,0 +1,41 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { fireEvent, render } from '@testing-library/react-native';
+import React from 'react';
+import { Linking, Text } from 'react-native';
+import Stack from '../navigation/Stack';
+import { LICENSES_SCREEN, ScreenName } from '../screens/names';
+import OthersPage from './OthersPage';
+
+it('provides link to third party screen', () => {
+  const licensesScreenContent = 'hello world';
+  const LicensesPage = () => <Text>{licensesScreenContent}</Text>;
+  const { getByText } = render(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={'default' as ScreenName} component={OthersPage} />
+        <Stack.Screen name={LICENSES_SCREEN} component={LicensesPage} />
+      </Stack.Navigator>
+    </NavigationContainer>,
+  );
+
+  fireEvent.press(getByText(/licenses/i));
+
+  expect(getByText(licensesScreenContent)).not.toBeNull();
+});
+
+it.each([['source code'], ['Buy Me a Coffee']])(
+  'provides link to %s',
+  title => {
+    const openUrl = jest.fn(() => Promise.resolve(''));
+    jest.spyOn(Linking, 'openURL').mockImplementation(openUrl);
+    const { getByText } = render(
+      <NavigationContainer>
+        <OthersPage />
+      </NavigationContainer>,
+    );
+
+    fireEvent.press(getByText(new RegExp(title, 'i')));
+
+    expect(openUrl).toHaveBeenCalled();
+  },
+);
