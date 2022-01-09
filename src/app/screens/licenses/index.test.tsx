8afbf14cc1c8b3d15678dd0f53ba9aba297f7860
Wooseop Kim <wooseop.kim.dev@gmail.com>
2022-01-09 22:32:14 +0900

    Fill in empty LicensesScreen test suite

--- src/app/screens/licenses/index.test.tsx
+++ src/app/screens/licenses/index.test.tsx
@@ -1,7 +1,9 @@
 import { render } from '@testing-library/react-native';
 import React from 'react';
-import { View } from 'react-native';
+import LicensesScreen from '.';
 
-it('', () => {
-  render(<View />);
+it('displays license data', () => {
+  const { getByA11yLabel } = render(<LicensesScreen />);
+
+  expect(getByA11yLabel(/^react-native@/)).toBeTruthy();
 });
