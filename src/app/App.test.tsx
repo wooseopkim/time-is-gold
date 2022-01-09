e10466f061610b2fdeed791f6c25485cbc28869c
Wooseop Kim <wooseop.kim.dev@gmail.com>
2022-01-09 22:16:47 +0900

    Remove App test so that coverage could be more correct

--- src/app/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react-native';
-import React from 'react';
-import App from './App';
-
-test('default screen is game screen', () => {
-  const { getByText } = render(<App />);
-
-  expect(getByText(/Enjoy Time Is Gold/)).toBeTruthy();
-});
